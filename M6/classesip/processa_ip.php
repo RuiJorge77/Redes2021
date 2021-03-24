e62a40770b7a222b80743086e2d11a096b208bbf
RuiJorge77 <72562395+RuiJorge77@users.noreply.github.com>
2021-03-24 12:30:42 +0000

    update

--- M6/classesip/processa_ip.php
+++ M6/classesip/processa_ip.php
@@ -1,23 +1,59 @@
 <?php
-    include "css.php";
-    if($_GET['a'] <= 255 && $_GET['b'] <= 255 && $_GET['c'] <= 255 && $_GET['d'] <= 255 && $_GET['a'] >= 0 && $_GET['b'] >= 0 && $_GET['c'] >= 0 && $_GET['d'] >= 0){
-        $ip = array(
-            'a' => $_GET['a'],
-            'b' => $_GET['b'],
-            'c' => $_GET['c'],
-            'd' => $_GET['d'],
-        );
+    include "css.php";     
+            $a = $_GET['a'];
+            $b = $_GET['b'];
+            $c = $_GET['c'];
+            $d = $_GET['d'];
+    if($a > 255 || $a < 0 || $b > 255 || $b < 0 || $c > 255 || $c < 0 || $d > 255 || $d < 0){
+        echo 'ip Invalido. Digite um endereço IP entre 0 e 255';
+    }
+    else if($a == 127) {
+        echo 'IP reservado para loopback ou localhost';
+    }
+    else if ($a <= 126 && $a > 0 && $b >= 0 && $b <= 255 && $c >= 0 && $c <= 255 && $d >= 1 && $d <= 254){
+        echo 'Classe A';
+    }
+    else if ($a >= 128 && $a <= 191 && $b >= 1 && $b <= 255 && $c >= 0 && $c <= 255 && $d >= 1 && $d <= 254){
+        echo 'Classe B';
+    }
+    else if ($a >= 192 && $a <= 223 && $b >= 0 && $b <= 255 && $c >= 1 && $c <= 254 && $d >= 1 && $d <= 254){
+        echo 'Classe C';
+    }
+    else if ($a >= 224 && $a <= 239 && $b >= 0 && $b <= 255 && $c >= 0 && $c <= 255 && $d >= 0 && $d <= 255){
+        echo 'Classe D';
+    }
+    else if ($a >= 240 && $a <= 254 && $b >= 0 && $b <= 255 && $c >= 0 && $c <= 255 && $d >= 0 && $d <= 254){
+        echo 'Classe E';
     }
     else{
-        echo  "IP Inválido";
-        return;
+		echo "IP inválido";
+	}
+    
+     echo "<br>";
+    if($a == 10){
+        echo "IP Privado";
     }
-    $ipfim = implode(".",$ip);    
-    ip_in_range("1.0.0.1", "126.255.255.254", $ipFim);
-    ip_in_range("128.1.0.1", "191.255.255.254", $ipFim);
-    ip_in_range("192.0.0.1", "223.255.255.254", $ipFim);
-    ip_in_range("224.0.0.1", "239.255.255.254", $ipFim);
-    ip_in_range("240.0.0.1", "254.255.255.254", $ipFim);
+    elseif($a == 172 && ($b >= 16 && $b <= 1)){
+        echo "IP Privado";
+    }
+    elseif($a == 192 && $b == 168 && ($c > 0 && $c <= 255)){
+        echo "IP Privado";
+    }
+     elseif($a == 255){
+        echo "IP Invalido";
+    }
+    elseif($b == 0 && $c == 0 && $d == 0){
+        echo "IP Invalido";
+    }
+    elseif($c == 0 && $d == 0){
+        echo "IP Invalido";
+    }
+    elseif($d == 0 && $d == 255){
+        echo "IP Invalido";
+    }
+    else{
+        echo "IP publico";
+    }
 ?>
 <br>
 <br>
